2ab53272fb8de91e00498613791047a000908f34
FirsT2015 <you@example.com>
2024-01-08 12:20:46 +0100

    eslint error

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,14 +2,16 @@
 require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
-  root: true,
-  'extends': [
-    'plugin:vue/vue3-essential',
-    'eslint:recommended',
-    '@vue/eslint-config-typescript',
-    '@vue/eslint-config-prettier/skip-formatting'
-  ],
-  parserOptions: {
-    ecmaVersion: 'latest'
-  }
+ root: true,
+ 'extends': [
+   'plugin:vue/vue3-essential',
+   'eslint:recommended',
+   '@vue/eslint-config-typescript',
+   '@vue/eslint-config-prettier/skip-formatting'
+ ],
+ parserOptions: {
+   ecmaVersion: 'latest',
+   project: './tsconfig.json',
+   tsconfigRootDir: __dirname
+ }
 }
